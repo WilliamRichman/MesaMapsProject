a32af0c6d09dd8fe4bce4c00c6cfb9feb84d0207
William Richman <WRichma@mesaaz.gov>
2019-11-14 09:59:28 -0700

    First Push

---
+++ scritp.js
@@ -0,0 +1,9 @@
+/*
+for Swipe on images 
+*/
+
+const _C = document.querySelector('.container'),
+    N = _C.children.length;
+
+_C.getElementsByClassName.setProperty('--n', N)
+
---
+++ scss/script.js
@@ -0,0 +1,4 @@
+const _C = document.querySelector('.container'),
+    N = _C.children.length;
+
+_C.style.setProperty('--n', N)
